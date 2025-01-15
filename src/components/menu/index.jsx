81683320606cd0754404337eaffdcd35623fc49c
param9478 <singh.parminder9478@gmail.com>
2025-01-15 13:31:50 -0700

    fixing error

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,5 +1,5 @@
 import Header from '../header';
-import PDFMenuViewer from './PDFMenuViewer'; // Import the PDFMenuViewer component
+import PDFMenuViewer from './PdfMenuViewer'; // Import the PDFMenuViewer component
 import MenuFile from '../../assets/menu-image.pdf';
 
 export default function Menu() {
@@ -8,7 +8,7 @@ export default function Menu() {
       <Header
         title="Our Menu"
         content="Explore our carefully curated selection of dishes"
-        imgSrc="https://images.unsplash.com/photo-1511978293554-7b92f19bd77d?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        imgSrc="https://images.unsplash.com/photo-1533777857889-4be7c70b33f7?q=80&w=3570&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         fullWidth
       />
       <PDFMenuViewer pdfFile={MenuFile} />
